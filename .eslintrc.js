a2f38585f397f5866dec2e5748124cecd77c4fa0
Ahmad Madany <ahmad.madany.01@gmail.com>
2022-09-21 05:56:15 +0300

    Generate boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'airbnb-base', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+}
